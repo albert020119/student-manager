6e14f96027792080e994f158098e10957d74c51e
Albert Endre <albert_endre@yahoo.com>
2022-03-27 16:10:58 +0300

    fixed shit

--- src/main/java/loose/oose/fis/lab/student/manager/HelloApplication.java
+++ src/main/java/loose/oose/fis/lab/student/manager/HelloApplication.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
